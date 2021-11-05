7f35d7c2f83855a1b29b955cef79d376e07b6d6e
Eum Young Hyun <opwer032@naver.com>
2021-11-05 23:50:33 +0900

    feat: Add ref and calculate cube size

--- dongnae/src/pages/home/controller.tsx
+++ dongnae/src/pages/home/controller.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Presenter from './presenter';
 
 function Container() {
-    return <Presenter />;
+    const [cubeHeight, setCubeHeight] = useState(0);
+    const wrapperRef = useRef<HTMLDivElement>(null);
+    const cubeContRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (cubeContRef.current) setCubeHeight(cubeContRef.current.offsetHeight);
+    }, []);
+
+    return <Presenter cubeHeight={cubeHeight} wrapperRef={wrapperRef} cubeContRef={cubeContRef} />;
 }
 
 export default Container;
