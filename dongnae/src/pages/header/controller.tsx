6eccc42baa872189ea43828553e3e23972d02675
Eum Young Hyun <opwer032@naver.com>
2021-11-07 16:50:44 +0900

    feat: Devide header for responsive screen

--- dongnae/src/pages/header/controller.tsx
+++ dongnae/src/pages/header/controller.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import Presenter from './presenter';
 
-import { screenTypeState } from '../../modules/recoil/screenType';
-import { useRecoilValue } from 'recoil';
-
 function Container() {
-    // const screenType = useRecoilValue(screenTypeState);
-
     //TODO: pc) 상단 top 10% hover시 헤더 등장, 평상시에는 로고만 보이게
     //          add event listner mousemove, mouse y값과 window.innerheight 비교
     //      태블릿, 모바일) 상단으로 스크롤 시 헤더 등장, 평상시에는 로고만 보이게
--- dongnae/src/pages/header/presenter.tsx
+++ dongnae/src/pages/header/presenter.tsx
@@ -1,25 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
+
+import { screenTypeState } from '../../modules/recoil/screenType';
+import Logo from '../../components/icons/logo';
 
 function Presenter() {
+    const screenType = useRecoilValue(screenTypeState);
+
     return (
-        <Container>
-            <h2>Header</h2>
-        </Container>
+        <>
+            <LogoContainer>
+                <Logo />
+            </LogoContainer>
+            {screenType === 'isPC' ? (
+                <Desktop>
+                    <h2>desktop Header</h2>
+                </Desktop>
+            ) : (
+                <Mobile>
+                    <h2>mobile Header</h2>
+                </Mobile>
+            )}
+        </>
     );
 }
 
 export default Presenter;
 
+const LogoContainer = styled.div`
+    width: 100%;
+    height: 10%;
+
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    background: transparent;
+    z-index: 3;
+`;
 const Container = styled.div`
     width: 100%;
     height: 10%;
 
     position: fixed;
-    top: -10%;
+    top: 0;
     left: 0;
 
     background: red;
     transition: 0.8s ease-out;
     z-index: 2;
+`;
+
+const Desktop = styled(Container)`
+    background: blue;
+`;
+const Mobile = styled(Container)`
+    background: green;
 `;
